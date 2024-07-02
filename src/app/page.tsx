a6bc58921cc197b6fd70d40130591a55a21dd8bd
computerkingminsu <ya5656@naver.com>
2024-07-02 15:23:20 +0900

    feat: add user status input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { countriesHex } from '../app/countriesHex';
 import { countriesData } from '../app/countriesData';
 import { useRouter } from 'next/navigation';
 import { useWindowSize } from '@react-hook/window-size/throttled';
+import { IoMicOutline } from 'react-icons/io5';
+import { LuSend } from 'react-icons/lu';
 
 export default function GlobeEdit() {
   const router = useRouter();
@@ -20,7 +22,7 @@ export default function GlobeEdit() {
     globeRef.current.pointOfView({
       lat: 35.907757,
       lng: 127.766922,
-      altitude: 1.75,
+      altitude: 2,
     });
   }, []);
 
@@ -69,7 +71,7 @@ export default function GlobeEdit() {
       {
         lat: 35.907757,
         lng: 127.766922,
-        altitude: 1.75,
+        altitude: 2,
       },
       1000
     ); // Transition back to the initial view
@@ -90,36 +92,52 @@ export default function GlobeEdit() {
   );
 
   return (
-    <div className="relative">
-      <Globe
-        ref={globeRef}
-        width={width > 480 ? width : 480}
-        height={height}
-        labelsData={countriesDataValues}
-        labelText={(d) => d.name}
-        labelSize={1.5} // Increase label size
-        labelDotRadius={useCallback(() => 1.8, [])}
-        labelAltitude={useCallback(() => 0.01, [])}
-        labelColor={useCallback(() => '#ffd000', [])}
-        labelsTransitionDuration={500}
-        hexPolygonsData={countriesHex.features}
-        hexPolygonResolution={useCallback(() => 3, [])}
-        hexPolygonMargin={useCallback(() => 0.4, [])}
-        hexPolygonColor={useCallback(() => '#2e7a7c', [])}
-        backgroundColor={'#222534'}
-        showGlobe={false}
-        showAtmosphere={false}
-        onLabelClick={handleLabelClick}
-        onLabelHover={handleLabelHover}
-      />
-      {labelToShow && (
-        <div className="absolute top-[35%] left-1/2 transform -translate-x-1/2 bg-white p-5 rounded-lg z-10">
-          <h3>{selectedLabel}</h3>
-          <button className="mt-2 bg-blue-500 text-white px-3 py-1 rounded" onClick={handleBackClick}>
-            Back
-          </button>
+    <>
+      <div>
+        <Globe
+          ref={globeRef}
+          width={width > 480 ? width : 480}
+          height={height}
+          labelsData={countriesDataValues}
+          labelText={(d) => d.name}
+          labelSize={1.5} // Increase label size
+          labelDotRadius={useCallback(() => 1.8, [])}
+          labelAltitude={useCallback(() => 0.01, [])}
+          labelColor={useCallback(() => '#ffd000', [])}
+          labelsTransitionDuration={500}
+          hexPolygonsData={countriesHex.features}
+          hexPolygonResolution={useCallback(() => 3, [])}
+          hexPolygonMargin={useCallback(() => 0.4, [])}
+          hexPolygonColor={useCallback(() => '#2e7a7c', [])}
+          backgroundColor={'#222534'}
+          showGlobe={false}
+          showAtmosphere={false}
+          onLabelClick={handleLabelClick}
+          onLabelHover={handleLabelHover}
+        />
+        {labelToShow && (
+          <div className="absolute top-[35%] left-1/2 transform -translate-x-1/2 bg-white p-5 rounded-lg z-10">
+            <h3>{selectedLabel}</h3>
+            <button className="mt-2 bg-blue-500 text-white px-3 py-1 rounded" onClick={handleBackClick}>
+              Back
+            </button>
+          </div>
+        )}
+
+        <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 w-full max-w-lg ">
+          <div className="flex items-center bg-gray-700 text-white rounded-full p-2">
+            <input
+              type="text"
+              className="flex-grow bg-transparent border-none outline-none text-white placeholder-gray-400 px-4"
+              placeholder="당신의 예산, 일정, 취향 등을 입력해주세요."
+            />
+            <div className="flex items-center space-x-2 mr-4">
+              <IoMicOutline size={30} className="cursor-pointer text-gray-400" />
+              <LuSend size={25} className="cursor-pointer text-gray-400" />
+            </div>
+          </div>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 }
