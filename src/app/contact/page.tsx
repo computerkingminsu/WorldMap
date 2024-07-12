904040ac72ebd3e90916dd63e49e656540408b99
computerkingminsu <ya5656@naver.com>
2024-07-12 22:55:39 +0900

    feat: contact page bg color

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,14 @@
 export default function Contact() {
   return (
     <>
-      <div className="w-screen h-screen bg-[#222534]">
+      <div className="w-screen h-screen bg-[#151825]">
         <div className="flex justify-center items-center h-full flex-col">
-          <div className="text-white text-2xl font-semibold">email : computerking0646@gmail.com</div>
-          <div className="text-white text-2xl font-semibold">github : https://github.com/computerkingminsu</div>
+          <div className="text-white text-2xl font-semibold">
+            email : computerking0646@gmail.com
+          </div>
+          <div className="text-white text-2xl font-semibold">
+            github : https://github.com/computerkingminsu
+          </div>
         </div>
       </div>
     </>
